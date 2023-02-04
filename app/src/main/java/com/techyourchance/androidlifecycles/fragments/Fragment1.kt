81363a467e780a7eca1288aac7805dc31fe77c08
Vasiliy Zukanov <techyourchance@gmail.com>
2023-02-04 11:09:20 +0200

    Overriging and logging onViewCreated in Fragment1

--- app/src/main/java/com/techyourchance/androidlifecycles/fragments/Fragment1.kt
+++ app/src/main/java/com/techyourchance/androidlifecycles/fragments/Fragment1.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.techyourchance.androidlifecycles.R
 import timber.log.Timber
@@ -14,7 +16,16 @@ class Fragment1: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Timber.i("onCreateView()")
-        return layoutInflater.inflate(R.layout.fragment_1, container, false)
+        return layoutInflater.inflate(R.layout.fragment_1, container, false).apply {
+            findViewById<Button>(R.id.btnNextFragment).setOnClickListener {
+                Toast.makeText(requireContext(), "TODO", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Timber.i("onViewCreated()")
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
