d32aa68f33154d0608dc90bd29b9ed25d1cf9ac3
Vasiliy Zukanov <techyourchance@gmail.com>
2023-02-08 17:14:43 +0200

    Employing FragmentLifecycleCallbacks for extended lifecycle logging

--- app/src/main/java/com/techyourchance/androidlifecycles/fragments/FragmentHostActivity.kt
+++ app/src/main/java/com/techyourchance/androidlifecycles/fragments/FragmentHostActivity.kt
@@ -4,8 +4,11 @@ import android.app.Application
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.techyourchance.androidlifecycles.BackgroundDetector
 import com.techyourchance.androidlifecycles.CustomApplication
 import com.techyourchance.androidlifecycles.R
@@ -45,6 +48,8 @@ class FragmentHostActivity : AppCompatActivity() {
         }
 
         updateButtonText()
+
+        supportFragmentManager.registerFragmentLifecycleCallbacks(MyFragmentLifecycleCallbacks(), false)
     }
 
     private fun updateButtonText() {
@@ -107,5 +112,82 @@ class FragmentHostActivity : AppCompatActivity() {
             val intent = Intent(context, FragmentHostActivity::class.java)
             context.startActivity(intent)
         }
+    }
+
+}
+
+private class MyFragmentLifecycleCallbacks : FragmentManager.FragmentLifecycleCallbacks() {
+    override fun onFragmentPreAttached(fm: FragmentManager, f: Fragment, context: Context) {
+        super.onFragmentPreAttached(fm, f, context)
+        Timber.d("onFragmentPreAttached(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentAttached(fm: FragmentManager, f: Fragment, context: Context) {
+        super.onFragmentAttached(fm, f, context)
+        Timber.d("onFragmentAttached(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentPreCreated(fm: FragmentManager, f: Fragment, savedInstanceState: Bundle?) {
+        super.onFragmentPreCreated(fm, f, savedInstanceState)
+        Timber.d("onFragmentPreCreated(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentCreated(fm: FragmentManager, f: Fragment, savedInstanceState: Bundle?) {
+        super.onFragmentCreated(fm, f, savedInstanceState)
+        Timber.d("onFragmentCreated(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentActivityCreated(fm: FragmentManager, f: Fragment, savedInstanceState: Bundle?) {
+        super.onFragmentActivityCreated(fm, f, savedInstanceState)
+        Timber.d("onFragmentActivityCreated(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentViewCreated(fm: FragmentManager, f: Fragment, v: View, savedInstanceState: Bundle?) {
+        super.onFragmentViewCreated(fm, f, v, savedInstanceState)
+        Timber.d("onFragmentViewCreated(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentStarted(fm: FragmentManager, f: Fragment) {
+        super.onFragmentStarted(fm, f)
+        Timber.d("onFragmentStarted(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentResumed(fm: FragmentManager, f: Fragment) {
+        super.onFragmentResumed(fm, f)
+        Timber.d("onFragmentResumed(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentPaused(fm: FragmentManager, f: Fragment) {
+        super.onFragmentPaused(fm, f)
+        Timber.d("onFragmentPaused(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentStopped(fm: FragmentManager, f: Fragment) {
+        super.onFragmentStopped(fm, f)
+        Timber.d("onFragmentStopped(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentSaveInstanceState(fm: FragmentManager, f: Fragment, outState: Bundle) {
+        super.onFragmentSaveInstanceState(fm, f, outState)
+        Timber.d("onFragmentSaveInstanceState(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentViewDestroyed(fm: FragmentManager, f: Fragment) {
+        super.onFragmentViewDestroyed(fm, f)
+        Timber.d("onFragmentViewDestroyed(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentDestroyed(fm: FragmentManager, f: Fragment) {
+        super.onFragmentDestroyed(fm, f)
+        Timber.d("onFragmentDestroyed(): ${getFragmentName(f)}")
+    }
+
+    override fun onFragmentDetached(fm: FragmentManager, f: Fragment) {
+        super.onFragmentDetached(fm, f)
+        Timber.d("onFragmentDetached(): ${getFragmentName(f)}")
+    }
+
+    fun getFragmentName(f: Fragment): String {
+        return f.javaClass.simpleName
     }
 }
